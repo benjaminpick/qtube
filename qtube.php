6ea22628657b43c0e0870b341595c8d60164e145
morl99 <choffmann87@gmail.com>
2015-07-19 20:14:16 +0200

    Changed API to iFrame instead of object embedding. Removed deprecated parameters.

--- qtube.php
+++ qtube.php
@@ -2,7 +2,7 @@
 /**
  * QTube Plugin
  *
- * @version 1.0.1
+ * @version 1.1.0
  * @package qtube
  * @author Massimo Giagnoni
  * @copyright Copyright (C) 2008 Massimo Giagnoni. All rights reserved.
@@ -39,7 +39,7 @@ class plgContentQTube extends JPlugin {
 			if(array_search('debug', $m[1]) !== false) {
 				return '{qtube ' . str_replace(' debug:=1', '', $matches[1]) . '}';
 			}
-			$attrs = array('vid'=>'', 'w'=>'', 'h'=>'', 'rel'=>'', 'c1'=>'', 'c2'=>'', 'b'=>'', 'ap'=>'', 'hd'=>'', 'dc'=>'', 'cl'=>'', 'id'=>'');
+			$attrs = array('vid'=>'', 'w'=>'', 'h'=>'', 'c1'=>'', 'c2'=>'', 'b'=>'', 'ap'=>'', 'hd'=>'', 'dc'=>'', 'id'=>'');
 			for($i=0; $i < $r; $i++) {
 				$n= $m[1][$i];
 				$v = $m[2][$i] ? $m[2][$i] : $m[3][$i];
@@ -47,12 +47,12 @@ class plgContentQTube extends JPlugin {
 					$attrs[$n] = htmlspecialchars($v);
 				}
 			}
+
+			$attrs['url'] = 'http://www.youtube.com/embed/';
+
 			foreach($attrs as $n=>$v) {
 				if($v == '') {
 					switch($n) {
-						case 'cl':
-						$v = $params->get('class', '');	
-						break;
 						case 'id':
 						$v = $params->get('id', '');	
 						break;
@@ -62,26 +62,8 @@ class plgContentQTube extends JPlugin {
 						case 'h':
 						$v = $params->get('height', '355');
 						break;
-						case 'rel':
-						$v = $params->get('related', 1);
-						break;
-						case 'c1':
-						$v = $params->get('color1', '');
-						break;
-						case 'c2':
-						$v = $params->get('color2', '');
-						break;
-						case 'b':
-						$v = $params->get('border', 0);
-						break;
 						case 'ap':
 						$v = $params->get('autoplay', 0);
-						break;
-						case 'hd':
-						$v = $params->get('hd', 0);
-						break;
-						case 'dc':
-						$v = $params->get('dc', 0);
 						break;
 					}
 					$attrs[$n] = $v;
@@ -89,9 +71,6 @@ class plgContentQTube extends JPlugin {
 			}
 			foreach($attrs as $n=>$v) {
 				switch($n) {
-					case 'cl':
-					if($v != '') { $attrs[$n]= ' class="' . $v . '"'; }
-					break;
 					case 'id':
 					if($v != '') { $attrs[$n]= ' id="' . $v . '"'; }
 					break;
@@ -101,18 +80,6 @@ class plgContentQTube extends JPlugin {
 					case 'h':
 					$attrs[$n] = ' height="' . $v . '"';
 					break;
-					case 'rel':
-					$attrs[$n] = "&amp;rel=$v";
-					break;
-					case 'c1':
-					if($v != '') { $attrs[$n] = "&amp;color1=0x$v"; }
-					break;
-					case 'c2':
-					if($v != '') { $attrs[$n] = "&amp;color2=0x$v"; }
-					break;
-					case 'b':
-					$attrs[$n] = "&amp;border=$v";
-					break;
 					case 'ap':
 					if($v > 0) { 
 						$attrs[$n] = "&amp;autoplay=$v"; 
@@ -120,30 +87,15 @@ class plgContentQTube extends JPlugin {
 						$attrs[$n] = '';
 					}
 					break;
-					case 'hd':
-					if($v > 0) { 
-						$attrs[$n] = "&amp;hd=$v"; 
-					} else {
-						$attrs[$n] = '';
-					}
-					break;
-					case 'dc':
-						if($v > 0) {
-							$attrs[$n] = 'http://www.youtube-nocookie.com/v/';
-						} else {
-							$attrs[$n] = 'http://www.youtube.com/v/';
-						}
-					break;
 				}
 			}
 			
 			if($attrs['vid'] == '') {
 				$r = '{qtube error: video id missing!}';
 			} else {
-				$url = $attrs['dc'].$attrs['vid'].'&amp;fs=1'.$attrs['rel'].$attrs['c1'].$attrs['c2'].$attrs['b'].$attrs['hd'].$attrs['ap'];
+				$url = $attrs['url'].$attrs['vid'].'&amp;fs=1'.$attrs['ap'];
 				$r = <<<EOD
-<object{$attrs['cl']}{$attrs['id']} type="application/x-shockwave-flash"{$attrs['w']}{$attrs['h']} data="$url">
-<param name="movie" value="$url"></param><param name="wmode" value="transparent"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param></object>
+<iframe{$attrs['id']} type="text/html"{$attrs['w']}{$attrs['h']} src="$url" frameborder="0">
 EOD;
 			}
 			return $r;
